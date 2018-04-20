ffe07335629d30c3b54344fba9a0290ea79372c4
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2018-04-19 21:15:33 -0500

    Update.

--- listen_and_predict.py
+++ listen_and_predict.py
@@ -14,7 +14,8 @@ RECORD_DURATION_SECONDS = 3
 AIY_PROJECTS_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-tf_model_path = './safesense/tensorflow/yt8m'
+# tf_model_path = './safesense/tensorflow/yt8m'
+tf_model_path = 'safesense/tensorflow/'
 
 def make_spectrogram():
     """record a few secs of audio with the mic, convert to spectrogram"""
